6b7e7b333ab6cfaafda6ffd969b2510af4729c94
Your Name <you@example.com>
2026-06-09 12:34:02 +0200

    chore: add link in auth routes

--- client/src/components/ui/layout.tsx
+++ client/src/components/ui/layout.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@tanstack/react-router";
 import { Logo } from "../common/Logo";
 
 type Props = {
@@ -14,8 +15,9 @@ export function AuthLayout({ subtitle, children }: Props) {
         <div className="w-full max-w-md px-4 sm:px-6 py-10 flex flex-col items-center space-y-6 sm:space-y-8">
           {/* HEADER */}
           <div className="w-full flex flex-col items-center text-center space-y-3 mr-2">
-            <Logo />
-
+            <Link to="/">
+              <Logo />
+            </Link>
             {subtitle && <div className="text-tx-muted text-sm max-w-sm mx-auto leading-relaxed">{subtitle}</div>}
           </div>
 
